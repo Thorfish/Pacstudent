4f4bb753ba46fe18b203981adc6a6278868d31b0
Thorfish <yhal.liam@gmail.com>
2023-10-06 00:46:21 +1100

    Pacstudent Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public List<Transform> stops;
+    [SerializeField]
+    public float duration;
+
+    private float speed;
+    private Vector2 velocity;
+
+    private int target = 0;
+    private new Transform transform;
+    private Animator animator;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>(); 
+        animator = GetComponent<Animator>();
+        float distanceSum = 0;
+        for (int i = 1; i < stops.Count; i++)
+        {
+            distanceSum += Vector2.Distance(stops[i].position, stops[i - 1].position);
+        }
+        speed = distanceSum / duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = (stops[target].position - transform.position).normalized * speed;
+        if (Vector2.Distance(transform.position, stops[target].position) > speed)
+        {
+            transform.position = new Vector2(transform.position.x + velocity.x, transform.position.y + velocity.y);
+        }
+        else
+        {
+            transform.position = stops[target].position;
+            if (target == stops.Count - 1) { target = 0; }
+            else { target++; }
+        }
+    }
+}
